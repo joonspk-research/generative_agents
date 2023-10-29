f6ead72c0c3165ce6a50e0de01be61cecd8ddd1e
Muhammad Talha Khan <talhakhan325@gmail.com>
2023-10-29 16:02:24 +0500

    Updated run.bat
    added the code to make it more comprehensive.

--- run.bat
+++ run.bat
@@ -2,11 +2,11 @@
 
 REM START THE FIRST PYTHON SCRIPT IN THE FIRST FOLDER IN A NEW WINDOW
 
-start cmd /k "cd D:\generative_agents\environment\frontend_server && python manage.py runserver"
+start cmd /k "cd Add/path/to/folder && python manage.py runserver"
 
 REM START THE SECOND PYTHON SCRIPT IN THE SECOND FOLDER IN A NEW WINDOW
 
-start cmd /k "cd D:\generative_agents\reverie\backend_server && python reverie.py"
+start cmd /k "cd Add/path/to/folder && python reverie.py"
 
 REM PAUSE THIS SCRIPT TO KEEP THE CMD WINDOW OPEN
 
